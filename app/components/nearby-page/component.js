d856384d570f047574f2bad38e760fca5daa66b0
Richard Verheyen <richard@192-168-1-113.tpgi.com.au>
2019-12-31 11:37:46 +1100

    Add elements to display web geolocation permissions and success state

--- app/components/nearby-page/component.js
+++ app/components/nearby-page/component.js
@@ -1,5 +1,48 @@
 import Component from "@ember/component";
+import { set } from "@ember/object";
 
 export default Component.extend({
-  elementId: "nearby-page"
+  elementId: "nearby-page",
+  geolocationPermissionState: undefined,
+  latitude: undefined,
+  longitude: undefined,
+
+  init() {
+    this._super(...arguments);
+    this.updateGeolocationPermission();
+  },
+
+  updateGeolocationPermission() {
+    navigator.permissions.query({name:'geolocation'}).then(result => {
+      // result.state == 'granted'/'denied'/'prompt'(default)
+      set(this, 'geolocationPermissionState', result.state);
+    });
+  },
+
+  actions: {
+    alertGeolocation(){
+
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
+      const success = pos => {
+        this.updateGeolocationPermission();
+
+        window.alert(`${pos.coords.latitude}, ${pos.coords.longitude}`)
+        set(this, 'latitude', pos.coords.latitude);
+        set(this, 'longitude', pos.coords.longitude);
+      }
+
+      const error = err => {
+        this.updateGeolocationPermission();
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, options);
+
+    }
+  }
 });
