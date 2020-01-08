64687c979f5fd4d2aacf9dfd491fc061f9212eee
Richard Verheyen <richard@Richards-Air-2.gateway>
2020-01-08 14:08:14 +1100

    Move geo logic to it's own service

--- app/components/nearby-page/component.js
+++ app/components/nearby-page/component.js
@@ -1,48 +1,22 @@
 import Component from "@ember/component";
 import { set } from "@ember/object";
+import { inject as service } from '@ember/service';
+
 
 export default Component.extend({
   elementId: "nearby-page",
-  geolocationPermissionState: undefined,
-  latitude: undefined,
-  longitude: undefined,
+
+  geo: service(),
 
   init() {
     this._super(...arguments);
-    this.updateGeolocationPermission();
-  },
-
-  updateGeolocationPermission() {
-    navigator.permissions.query({name:'geolocation'}).then(result => {
-      // result.state == 'granted'/'denied'/'prompt'(default)
-      set(this, 'geolocationPermissionState', result.state);
-    });
+    this.geo.updateGeolocationPermission();
   },
 
   actions: {
-    alertGeolocation(){
-
-      const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      };
-
-      const success = pos => {
-        this.updateGeolocationPermission();
-
-        window.alert(`${pos.coords.latitude}, ${pos.coords.longitude}`)
-        set(this, 'latitude', pos.coords.latitude);
-        set(this, 'longitude', pos.coords.longitude);
-      }
-
-      const error = err => {
-        this.updateGeolocationPermission();
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-      }
-
-      navigator.geolocation.getCurrentPosition(success, error, options);
-
+    alertGeolocation() {
+      this.geo.displayGeolocation();
     }
   }
+
 });
