853059a48209a0470d8b491d4b0b50f874af6544
Richard Verheyen <richard@Richards-Air-2.gateway>
2020-01-11 22:24:57 +1100

    Add mapbox-gl package and test map

--- app/components/heatmap-page/component.js
+++ app/components/heatmap-page/component.js
@@ -1,5 +1,21 @@
 import Component from "@ember/component";
 
+import mapboxgl from "mapbox-gl";
+
 export default Component.extend({
-  elementId: "heatmap-page"
+  elementId: "heatmap-page",
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    var map = new mapboxgl.Map(
+      {
+        accessToken: 'pk.eyJ1IjoicmljaGFyZHZlcmhleWVuIiwiYSI6ImNqMms4ZjRmYTAwMHAzM285aXg0aXRtZmMifQ.UgSdnfuJU4EmzBlwHBWtDw',
+        container: 'test-map',
+        style: 'mapbox://styles/mapbox/basic-v9',
+        zoom: 13,
+        center: [ -96.7969879, 32.7766642 ]
+      }
+    );
+  },
 });
