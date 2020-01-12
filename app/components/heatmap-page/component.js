f78ab987fdbfb96597b4537886967e785a4f9712
Richard Verheyen <richard@Richards-MacBook-Air-2.local>
2020-01-13 09:11:34 +1100

    Move map over to the nearby page

--- app/components/heatmap-page/component.js
+++ app/components/heatmap-page/component.js
@@ -1,21 +1,7 @@
 import Component from "@ember/component";
-
+import { inject as service } from "@ember/service";
 import mapboxgl from "mapbox-gl";
 
 export default Component.extend({
   elementId: "heatmap-page",
-
-  didInsertElement() {
-    this._super(...arguments);
-
-    var map = new mapboxgl.Map(
-      {
-        accessToken: 'pk.eyJ1IjoicmljaGFyZHZlcmhleWVuIiwiYSI6ImNqMms4ZjRmYTAwMHAzM285aXg0aXRtZmMifQ.UgSdnfuJU4EmzBlwHBWtDw',
-        container: 'test-map',
-        style: 'mapbox://styles/mapbox/basic-v9',
-        zoom: 13,
-        center: [ -96.7969879, 32.7766642 ]
-      }
-    );
-  },
 });
--- app/components/nearby-page/component.js
+++ app/components/nearby-page/component.js
@@ -1,10 +1,30 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import mapboxgl from "mapbox-gl";
 
 class NearbyPage extends Component {
   elementId = 'nearby-page';
 
   @service geo;
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    if (this.geo.longitude) {
+      new mapboxgl.Map(
+        {
+          accessToken: 'pk.eyJ1IjoicmljaGFyZHZlcmhleWVuIiwiYSI6ImNrNWF4cGc1MDA0dGwzbG1zazUweWZmNjYifQ.b8WBF8yNZpC_qg9fiHMd8Q',
+          container: 'nearby-map',
+          style: 'mapbox://styles/mapbox/basic-v9',
+          zoom: 17,
+          center: [this.geo.longitude, this.geo.latitude ]
+        }
+      );
+    } else {
+      console.log('no geo data');
+    }
+
+  }
 }
 
 export default NearbyPage;
