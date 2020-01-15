2905fac13f151e172387809be8c402302ca0a66d
Richard Verheyen <richard@Richards-Air-2.modem>
2020-01-15 23:56:45 +1100

    Add pulsing dot to nearby map

--- app/components/nearby-page/map/component.js
+++ app/components/nearby-page/map/component.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import addPulsingDot from "./pulsingDot";
 import mapboxgl from "mapbox-gl";
 
 class NearbyMap extends Component {
@@ -8,15 +9,17 @@ class NearbyMap extends Component {
     this._super(...arguments);
 
     if (this.longitude) {
-      new mapboxgl.Map(
-        {
-          accessToken: 'pk.eyJ1IjoicmljaGFyZHZlcmhleWVuIiwiYSI6ImNrNWF4cGc1MDA0dGwzbG1zazUweWZmNjYifQ.b8WBF8yNZpC_qg9fiHMd8Q',
-          container: 'nearby-map',
-          style: 'mapbox://styles/mapbox/basic-v9',
-          zoom: 17,
-          center: [this.longitude, this.latitude ]
-        }
-      );
+      const map = new mapboxgl.Map({
+        accessToken: 'pk.eyJ1IjoicmljaGFyZHZlcmhleWVuIiwiYSI6ImNrNWF4cGc1MDA0dGwzbG1zazUweWZmNjYifQ.b8WBF8yNZpC_qg9fiHMd8Q',
+        container: 'nearby-map',
+        style: 'mapbox://styles/mapbox/basic-v9',
+        zoom: 17,
+        center: [this.longitude, this.latitude]
+      });
+
+      map.on('load', () => {
+        addPulsingDot(map, this.longitude, this.latitude);
+      });
     }
 
   }
