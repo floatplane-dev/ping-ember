148a7ec2ab792d1031f2d9dc182d476799dc215c
Richard Verheyen <richard@Richards-Air-2.modem>
2020-01-17 00:07:16 +1100

    Add bottom-navigation component to bring global navigation outside the router

---
+++ app/components/bottom-navigation/component.js
@@ -0,0 +1,8 @@
+import Component from "@ember/component";
+
+class BottomNavigation extends Component {
+  elementId = "bottom-navigation";
+  tagName = 'nav';
+};
+
+export default BottomNavigation;
