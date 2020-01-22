c84b863a7d1004b76bab4f8503c1074962118795
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-01-22 21:15:45 +1100

    Move user-profile and user-create over to <Page>

--- app/pods/components/bottom-navigation/component.js
+++
@@ -1,8 +0,0 @@
-import Component from "@ember/component";
-
-class BottomNavigation extends Component {
-  elementId = "bottom-navigation";
-  tagName = 'nav';
-};
-
-export default BottomNavigation;
--- app/pods/components/page/user-create/component.js
+++ app/pods/components/page/user-create/component.js
@@ -1,5 +1,28 @@
 import Page from '../component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class UserCreatePage extends Page {
   elementId = 'user-create';
+
+  @service auth;
+  @service router;
+
+  @action
+  create(user) {
+    console.debug('creating user...');
+    user
+      .save()
+      .then(response => {
+        console.debug('user created', { response });
+        const uuid = response.get('id');
+        localStorage.setItem('uuid', uuid);
+        console.debug('user UUID stored in localStorage');
+        this.auth.set('user', user);
+        this.router.transitionTo('user.profile');
+      })
+      .catch(response => {
+        console.error('failed to create user', { response });
+      });
+  }
 }
