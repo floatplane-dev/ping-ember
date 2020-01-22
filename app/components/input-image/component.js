131bfd924fc82a8892c837c436ac0d01e3580201
Richard Verheyen <richard@Richards-MacBook-Air-2.local>
2020-01-22 22:03:16 +1100

    Add naive input-image component to support image cropping and mobile interactions

--- app/components/input-image/component.js
+++ app/components/input-image/component.js
@@ -4,17 +4,53 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   tagName: "fieldset",
+  elementId: "input-image",
+  rawImage: undefined,
+  resizeParams: [],
 
   uploadImage: action(function(event) {
     const { user } = this;
     const file = event.target.files[0];
 
     const reader = new FileReader();
-    reader.onloadend = () => user.set('image', reader.result);
+    reader.onloadend = () => this.set('rawImage', reader.result);
     reader.readAsDataURL(file);
   }),
 
   clearImage: action(function(user) {
-    user.set('image', undefined);
-  })
+    user.set('rawImage', undefined);
+  }),
+
+  isMouseDown: false,
+
+  mouseDown(e) {
+    if (this.rawImage) {
+      console.log('mousedown');
+      e.preventDefault();
+      e.stopPropagation();
+      this.set('isMouseDown', true);
+    }
+  },
+
+  mouseUp(e) {
+    if (this.rawImage) {
+      console.log('mouseUp');
+      this.set('isMouseDown', false);
+    }
+  },
+
+  mouseLeave(e) {
+    if (this.rawImage) {
+      console.log('mouseLeave');
+      this.set('isMouseDown', false);
+    }
+  },
+
+  mouseMove(e) {
+    if (this.rawImage) {
+      if (this.isMouseDown) {
+        console.log([e.clientX || e.pageX, e.clientY || e.pageY]);
+      }
+    }
+  }
 });
---
+++ app/components/input-image/resizer/component.js
@@ -0,0 +1,29 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ["resizer"],
+  // isMouseDown: false,
+  //
+  // mouseDown(e) {
+  //   console.log('mousedown');
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  //   this.set('isMouseDown', true);
+  // },
+  //
+  // mouseUp(e) {
+  //   console.log('mouseUp');
+  //   this.set('isMouseDown', false);
+  // },
+  //
+  // mouseLeave(e) {
+  //   console.log('mouseLeave');
+  //   this.set('isMouseDown', false);
+  // },
+  //
+  // mouseMove(e) {
+  //   if (this.isMouseDown) {
+  //     console.log([e.clientX || e.pageX, e.clientY || e.pageY]);
+  //   }
+  // }
+});
